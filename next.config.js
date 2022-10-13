2b85e6734eec4cddf990c6971055e232fb2315b8
olimarie21 <olivia.underdah@gmail.com>
2022-10-12 18:27:13 -0700

    Config update with API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		MAPS_API_KEY: 'AIzaSyD2Pj5izCXUEL8GIharxviclrIKRK1JxrU',
+	},
 }
 
 module.exports = nextConfig
