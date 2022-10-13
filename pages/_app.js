0765f71b0dc8fd47c697a908f96ea912f419e164
olimarie21 <olivia.underdah@gmail.com>
2022-10-12 18:24:56 -0700

    Add material UI to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider } from '@emotion/react'
+import theme from '../src/Theme'
+import createEmotionCache from '../src/createEmotionCache'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+export default function MyApp(props) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	)
 }
 
-export default MyApp
+MyApp.propTypes = {
+	Component: PropTypes.elementType.isRequired,
+	emotionCache: PropTypes.object,
+	pageProps: PropTypes.object.isRequired,
+}
