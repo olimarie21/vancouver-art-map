baf0ed719d48c06f689f0f13a326e1249856b711
olimarie21 <olivia.underdah@gmail.com>
2022-12-29 20:20:09 -0800

    Adds Header to view

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import Head from 'next/head'
-import { Box, Container } from '@mui/material'
+import { Box } from '@mui/material'
 import MapWrapper from '../components/MapWrapper'
+import Header from '../components/Header'
 
 export default function Home() {
 	return (
-		<Box sx={{ margin: 0, padding: 0, height: '100vh', width: '100vw' }}>
+		<Box
+			sx={{
+				position: 'relative',
+				margin: 0,
+				padding: 0,
+				height: '100vh',
+				width: '100vw',
+			}}>
 			<Head>
 				<title>Vancity Public Art</title>
 				<meta name='description' content='Vancouver, B.C. Public Art' />
 				<link rel='icon' href='/favicon.ico' />
 				<meta name='viewport' content='initial-scale=1, width=device-width' />
 			</Head>
+			<Header />
 			<MapWrapper />
 		</Box>
 	)
