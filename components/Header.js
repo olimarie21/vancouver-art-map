8e04ecc96161d7763896022f4db92b6b13d2c1a1
olimarie21 <olivia.underdah@gmail.com>
2022-12-29 20:19:33 -0800

    Creates header component

---
+++ components/Header.js
@@ -0,0 +1,39 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+const Header = () => {
+	return (
+		<Container>
+			<H1>Vancouver Public Art</H1>
+		</Container>
+	)
+}
+
+const Container = styled(Box)(
+	({ theme }) => `
+	margin: 0;
+	padding: 0;
+	height: 10rem;
+    max-width: 90%;
+	background: ${theme.palette.primary.main};
+	display: flex;
+	justify-content: start;
+	align-items: center;
+    font-family: ${theme.typography.fontFamily};
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    border-radius: 0 0 2rem 0;
+`
+)
+
+const H1 = styled('h1')(
+	({ theme }) => `
+	margin: 0;
+	padding: 1rem 3rem;
+	color: ${theme.palette.text.light};
+`
+)
+
+export default Header
