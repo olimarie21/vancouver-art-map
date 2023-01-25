1a90ca2b166816abc8cf731d24a7b62cca4b84f3
olimarie21 <olivia.underdah@gmail.com>
2023-01-25 11:17:59 -0800

    Styling to index Container

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,12 @@ import Head from 'next/head'
 import { Box } from '@mui/material'
 import Header from '../components/Header'
 import Map from '../components/Map'
+import { styled } from '@mui/material/styles'
+import { Stack } from '@mui/system'
 
 export default function Home() {
 	return (
-		<Box
-			sx={{
-				position: 'relative',
-				margin: 0,
-				padding: 0,
-				height: '100vh',
-				width: '100vw',
-			}}>
+		<Container>
 			<Head>
 				<title>Vancity Public Art</title>
 				<meta name='description' content='Vancouver, B.C. Public Art' />
@@ -21,6 +16,18 @@ export default function Home() {
 			</Head>
 			<Header />
 			<Map />
-		</Box>
+		</Container>
 	)
 }
+
+const Container = styled(Stack)(
+	`
+	position: relative;
+	margin: 0;
+	padding: 0;
+	height: 100vh;
+	width: 100vw;
+
+	
+	`
+)
