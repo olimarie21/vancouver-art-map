ebcd1d442873aad30bc1c2c0edf3dbfb4742bd8e
olimarie21 <olivia.underdah@gmail.com>
2023-02-03 12:05:19 -0800

    Update btn hover styles

--- components/Buttons.js
+++ components/Buttons.js
@@ -7,11 +7,13 @@ export const PrimaryButton = styled(Button)(
 		border-radius: 0;
         background: ${theme.palette.secondary.main};
         color: ${theme.palette.text.dark};
-        font-size: 14px;
+        font-size: .85rem;
+        border: 1.5px solid ${theme.palette.secondary.main};
 
         &:hover {
-			background: ${theme.palette.primary.transparency};
-            color: ${theme.palette.secondary.main}
+			background: ${theme.palette.primary.main};
+            color: ${theme.palette.secondary.main};
+            border: 1.5px solid ${theme.palette.secondary.main};
 		}
     `
 )
@@ -22,10 +24,13 @@ export const SecondaryButton = styled(Button)(
 		border-radius: 0;
         background: ${theme.palette.primary.light};
         color: ${theme.palette.text.dark};
-        font-size: 14px;
+        font-size: .85rem;
+        border: 1.5px solid ${theme.palette.primary.light};
 
         &:hover {
-			background: ${theme.palette.secondary.light};
+			background: ${theme.palette.primary.main};
+            border: 1.5px solid ${theme.palette.primary.light};
+            color: ${theme.palette.text.light};
 		}
     `
 )
